a904d31d67c4a1562e631337b1b917c7a1be4df9
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-07-25 15:33:49 +0200

    simplify code a  bit and remove sqlite handler

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,30 @@ use nix_index::database::Reader;
 use regex::bytes::Regex;
 use serde::Serialize;
 
-fn dump_sqlite_to_stdout(db_path: &str) {
+#[tokio::main]
+async fn main() {
+    let mut args = env::args().skip(1);
+    let path = args
+        .next()
+        .expect("Please provide the path to the nix-index database");
+    let maybe_dump = args.next();
+
+    // dbg!(&path, &maybe_dump);
+    if let Some(flag) = maybe_dump {
+        if flag == "--dump-sqlite-fulltext-search" {
+            dump_sqlite_for_fulltext_search_to_stdout(&path);
+            return;
+        }
+    }
+
+    let db = Arc::new(path);
+    let app = Router::new().route("/query", get(query)).with_state(db);
+
+    let listener = tokio::net::TcpListener::bind("127.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn dump_sqlite_for_fulltext_search_to_stdout(db_path: &str) {
     use std::io::{self, Write};
     let reader = Reader::open(db_path).expect("Failed to open nix-index database");
     let stdout = io::stdout();
@@ -28,7 +51,6 @@ fn dump_sqlite_to_stdout(db_path: &str) {
     )
     .unwrap();
 
-    // Emit BEGIN;
     writeln!(handle, "BEGIN;").unwrap();
 
     let regex = Regex::new(".*").expect("Failed to compile regex");
@@ -47,35 +69,7 @@ fn dump_sqlite_to_stdout(db_path: &str) {
         }
     }
 
-    // Emit COMMIT;
     writeln!(handle, "COMMIT;").unwrap();
-}
-
-#[tokio::main]
-async fn main() {
-    let mut args = env::args().skip(1);
-    let path = args
-        .next()
-        .expect("Please provide the path to the nix-index database");
-    let maybe_dump = args.next();
-
-    // dbg!(&path, &maybe_dump);
-    if let Some(flag) = maybe_dump {
-        if flag == "--dump-sqlite" {
-            dump_sqlite_to_stdout(&path);
-            return;
-        }
-    }
-
-    let db = Arc::new(path);
-
-    let app = Router::new()
-        .route("/query", get(query))
-        .route("/dump-sqlite", get(dump_sqlite))
-        .with_state(db);
-
-    let listener = tokio::net::TcpListener::bind("127.1:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
 }
 
 async fn query(
@@ -114,31 +108,4 @@ async fn query(
 struct SearchResult {
     query: String,
     search_results: Vec<(String, String)>,
-}
-
-/// Handler to dump all nix-index entries as FTS5-compatible SQL insert statements
-async fn dump_sqlite(State(db_path): State<Arc<String>>) -> Response {
-    let reader = Reader::open(db_path.as_str()).expect("Failed to open nix-index database");
-
-    // Start with the CREATE VIRTUAL TABLE statement
-    let mut sql =
-        String::from("CREATE VIRTUAL TABLE entries USING FTS5(package_name, nix_path);\n");
-
-    // Iterate all entries in the database using a regex that matches everything
-    let regex = Regex::new(".*").expect("Failed to compile regex");
-    if let Ok(iter) = reader.query(&regex).run() {
-        for entry in iter {
-            if let Ok((store, file)) = entry {
-                let package_name = store.as_str().replace('\'', "''");
-                let nix_path = String::from_utf8_lossy(&file.path).replace('\'', "''");
-                sql.push_str(&format!(
-                    "INSERT INTO entries (package_name, nix_path) VALUES ('{}', '{}');\n",
-                    package_name, nix_path
-                ));
-            }
-        }
-    }
-
-    // Return as text/plain
-    ([(axum::http::header::CONTENT_TYPE, "text/plain")], sql).into_response()
 }
